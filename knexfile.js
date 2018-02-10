66656c9b9529429b83a384c61150d3dab8ea26e7
Brandon Fowler <bmfowler4@gmail.com>
2018-02-09 16:10:39 -1000

    added knex and created photos table

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'gallery_user',
+      password : 'password',
+      database : 'gallery',
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
